05df6733ae100d0ed79559350076f82f01f27fca
Anders Busch <andersbusch@gmail.com>
2019-06-23 16:21:16 +0200

    sticky tweening added

--- todo/include/Tweening.hpp
+++ todo/include/Tweening.hpp
@@ -8,6 +8,15 @@
 #include <functional>
 #include <array>
 
+/*
+ * The famous equality (closeness) check for Floating points.
+ * The tolerance here is measured in integer, and is prolly
+ * good enough since the results are converted to integer (pixel space)
+ * in the end
+ */
+bool isClose(double a, double b, int64_t tolerance = 1l) {
+    return std::abs(((int64_t)b) - ((int64_t)a)) <= tolerance;
+}
 
 struct Tweening2DPoint {
 
@@ -19,6 +28,27 @@ struct Tweening2DPoint {
     void fill(SDL_Rect &rect) {
         rect.x = x;
         rect.y = y;
+    }
+
+    /*
+     * Checks if the coordinates are close enough
+     * and if they are, basically jumps the last
+     * few pixels. Returns true iff it is considered close enough
+     */
+    bool stickyLerp(double f = 0, uint64_t pixelTolerance = 1) {
+        double t = 1.0 - f;
+        auto xClose = isClose(x, xNext, pixelTolerance);
+        auto yClose = isClose(y, yNext, pixelTolerance);
+
+        if ( !yClose || !xClose ) {
+            x = x * t + xNext * f;
+            y = y * t + yNext * f;
+        } else {
+            x = xNext;
+            y = yNext;
+        }
+
+        return xClose && yClose;
     }
 
     void lerp(double f = 0) {
