b15fb195b8d36fa16ac61a75d29d1b0c8d847e2b
Anders Busch <andersbusch@gmail.com>
2019-06-23 17:05:57 +0200

    plot code is cooler now

--- todo/stats/plot.py
+++ todo/stats/plot.py
@@ -3,47 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-
-filename = sys.argv[1]
-
-dataPath = pathlib.Path(filename)
-
-parsed = {
-    "frame": [],
-    "render": [],
-    "update": [],
-    "input": []
+data = {
+    "F": ([], "Frame time"),
+    "R": ([], "Render time"),
+    "U": ([], "Update time"),
+    "I": ([], "Input time"),
+    "L": ([], "Load time")
 }
 
 def parse(openedfile):
     for raw in openedfile:
-        key, _, value = raw.strip().partition(' ')
-        yield key, float(value)
+        key, delimiter, value = raw.strip().partition(' ')
+        if delimiter == ' ' and len(key) == 1 and len(value) > 0:
+            yield key.upper(), float(value)
 
-def getData(filepath: pathlib.Path):
+def fillData(filepath: pathlib.Path):
     with filepath.open(mode='r') as dataFile:
         for key, number in parse(dataFile):
-            if key == "F":
-                parsed["frame"].append(number)
-            if key == "U":
-                parsed["update"].append(number)
-            if key == "R":
-                parsed["render"].append(number)
-            if key == "I":
-                parsed["input"].append(number)
+            if data.get(key) is not None:
+                data[key][0].append(number)
 
-def plotit(data, title):
-    pl, = plt.plot(data, label=title)
-    plt.grid(True)
+def plotit(dataKey):
+    pl, = plt.plot(data[dataKey][0], label=data[dataKey][1])
     return pl
 
-if __name__ == "__main__":
-    getData(dataPath)
+def main():
+    if len(sys.argv) < 2:
+        print("Missing data file path")
+        exit(1)
+    filename = sys.argv[1]
+    dataPath = pathlib.Path(filename)
 
-    pl1 = plotit(parsed["frame"], "Frame time elapsed")
-    pl2 = plotit(parsed["render"], "Render time elapsed")
-    pl3 = plotit(parsed["update"], "Update time elapsed")
-    pl4 = plotit(parsed["input"], "Input time elapsed")
-
-    plt.legend([pl1, pl2, pl3, pl4], loc=1)
+    print("Parsing data...")
+    fillData(dataPath)
+    print("Plotting data...")
+    plotlines = tuple( plotit(key) for key in data.keys())
+    print("Rendering graphs...")
+    plt.grid(True, which='both')
+    plt.title("Game loop performance")
+    plt.ylabel("Time in milliseconds")
+    plt.xlabel("Frame sample count")
+    plt.legend(plotlines, loc=1)
     plt.show()
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
